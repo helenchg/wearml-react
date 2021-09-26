5e4463072ce04e3bdc43738422b317bc42dfa8f3
Elena Kodama <helenchg93@hotmail.com>
2021-09-26 19:40:11 -0400

    got rid of wearml to see what happens

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import Header from "./components/Header";
 import { Button } from "./components/Button-style";
 
 //import "wearml/wearml_engine";
-import wearML from "wearml/wearml_engine";
+//import wearML from "wearml/wearml_engine";
 
-window.wearML = wearML;
+//window.wearML = wearML;
 
 //wearML.setCommandSet("menu");
 const App = () => {
